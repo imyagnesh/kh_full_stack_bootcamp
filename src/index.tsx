53331a9d7a301262a90a52c6cc0903853c69e27f
Yagnesh Modh <hey@yagneshmodh.com>
2022-02-05 21:11:52 +0530

    commit code for state

--- src/index.tsx
+++ src/index.tsx
@@ -8,21 +8,74 @@ import './style.css';
 // 3. apply style as object with camelCase properties
 // 4. instead of class use classname to apply style
 
+// 1. Props are immutable
+
 const color = 'white';
 // Function Component
-const App = () => (
-  <>
-    <h1
-      style={{
-        color,
-        backgroundColor: 'red',
-      }}
-    >
-      Hello from react component
-    </h1>
-    <h2 className="heading">hello to check class</h2>
-    <input />
-  </>
-);
+
+// const App = ({ title, caption }: AppProps) => (
+//   <>
+//     <h1
+//       style={{
+//         color,
+//         backgroundColor: 'red',
+//       }}
+//     >
+//       {title}
+//     </h1>
+//     <h2 className="heading">{caption}</h2>
+//     <input type="checkbox" />
+//   </>
+// );
+
+// Class Component
+
+type AppProps = {};
+
+type AppState = {
+  counter: number;
+};
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  render(): React.ReactNode {
+    console.log('Render');
+
+    const { counter } = this.state;
+
+    return (
+      <>
+        <button
+          type="button"
+          onClick={() => {
+            // on set state method call render call again
+            this.setState({
+              counter: counter + 1,
+            });
+          }}
+        >
+          Increment
+        </button>
+        <h1>{counter}</h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              counter: counter - 1,
+            });
+          }}
+        >
+          Decrement
+        </button>
+      </>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
